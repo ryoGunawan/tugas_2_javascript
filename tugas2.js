7f557f1d46d4ef22529622cbc85c0468bc0fec0b
Suryo <ryogunawan1@gmail.com>
2020-05-07 14:48:38 +0700

    tugas 2 javascript
    Javascript Pengenalan

---
+++ tugas2.js
@@ -0,0 +1,16 @@
+var hp =085856000515;
+var nama ='suryo gunawan';
+var tinggi = true;
+  var cm = 170;
+    if (cm>160)
+      tinggi = true;
+      else
+      tinggi =false;
+var hidup ="iya";
+var email = "ryogunawan1@gmail.com";
+
+console.log(hp);
+console.log(nama);
+console.log(tinggi);
+console.log(hidup);
+console.log(email);
